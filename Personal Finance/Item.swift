ac89e0387fe298b061f5fab5a53492a04e76217a
Francesco Bianco <francesco.bianco@me.com>
2025-08-24 14:41:54 +0200

    Initial Commit

---
+++ Personal Finance/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Personal Finance
+//
+//  Created by Francesco Bianco on 24/08/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Personal FinanceTests/Personal_FinanceTests.swift
@@ -0,0 +1,17 @@
+//
+//  Personal_FinanceTests.swift
+//  Personal FinanceTests
+//
+//  Created by Francesco Bianco on 24/08/25.
+//
+
+import Testing
+@testable import Personal_Finance
+
+struct Personal_FinanceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
